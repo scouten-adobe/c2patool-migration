ce258f1aed55775b7da49e5af9ea409e813bb850
Eric Scouten <scouten@adobe.com>
2024-12-09 21:34:55 -0800

    Rename tags to match the pattern that release-plz generates

--- migrate.sh
+++ migrate.sh
@@ -39,6 +39,13 @@ git branch -D hack_tree_dump # last updated 2022-12-12
 git branch -D per-commit-builds # last updated 2023-05-17
 git branch -D update-cargo-deny # @scouten last updated 2024-03-28
 
+# All release tags in the c2patool repo will be renamed from "v0.9.11" (as an example) to "c2patool-v0.9.11" to match the tag format used by release-plz.
+for tag in $(git tag); do
+    new_tag="c2patool-$tag"
+    git tag "$new_tag" "$tag"
+    git tag -d "$tag"
+done
+
 # The root of the c2patool repo will be moved down a level and placed in a folder named cli.
 # The GitHub workflows will be exempted from this and will be separately merged into c2pa-rs's .github folder.
 FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch \
@@ -49,7 +56,6 @@ FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch \
 # View resulting repo in SourceTree.
 open -a /Applications/SourceTree.app/ .
 
-# All release tags in the c2patool repo will be renamed from "v0.9.11" (as an example) to "c2patool-v0.9.11" to match the tag format used by release-plz.
 # Fix broken links in c2patool's CHANGELOG.md which (incorrectly) point to c2pa-rs repo PRs.
 # Commit comments that reference issues in the c2patool repo will be rewritten to point to PRs and issues in the c2patool repo (i.e. the #xxx syntax will be rewritten to say content-auth/c2patool#xxx so that the existing discussion remains).
 echo "Not done yet"
